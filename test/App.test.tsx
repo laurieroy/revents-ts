4a905d82160ec3a4c3aac28005bea3ef0c87498d
Laurie Roy <laurieroy.dev@gmail.com>
2023-10-11 16:26:00 -0500

    Clean up boilerplate, add folders

--- test/App.test.tsx
+++ test/App.test.tsx
@@ -1,10 +1,10 @@
 import { render, screen } from "@testing-library/react";
-import App from "../src/App";
+import App from "../src/app/layout/App";
 
 describe("App", () => {
   it("renders headline", () => {
     render(<App />);
-    const headline = screen.getByText(/It works and you found me!/i);
+    const headline = screen.getByText(/Welcome to Revents/i);
     expect(headline).toBeInTheDocument();
   });
 });
